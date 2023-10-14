74c14c75b33fc93f9089f575c752b6e46d398253
CssIsEasy <147370104+cssiseasy@users.noreply.github.com>
2023-10-14 20:15:51 +0300

    Update script.js

--- Input Animation/script.js
+++ Input Animation/script.js
@@ -1,3 +1,4 @@
+/*The password display feature was added at the request of my followers, it's not necessary for the animation to work*/
 function myFunction() {
     var x = document.getElementById("passwordInput");
     if (x.type === "password") {
