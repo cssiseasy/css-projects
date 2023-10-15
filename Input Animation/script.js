4f2ca0e122673597f65874974f12c35be839316d
CssIsEasy <147370104+cssiseasy@users.noreply.github.com>
2023-10-15 10:33:59 +0300

    Delete Input Animation/script.js

--- Input Animation/script.js
+++
@@ -1,9 +0,0 @@
-/*The password display feature was added at the request of my followers, it's not necessary for the animation to work*/
-function myFunction() {
-    var x = document.getElementById("passwordInput");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
